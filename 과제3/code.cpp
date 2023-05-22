3b0f18786352d0798c8e747fb8129df732795d0f
tbutc <91929683+tbutc@users.noreply.github.com>
2023-05-23 01:03:57 +0900

    Add files via upload

--- 과제3/code.cpp
+++ 과제3/code.cpp
@@ -18,6 +18,78 @@ void program_exit();
 // 변수 선언
 FILE* in_fp, *out_fp;
 
+
+
+/*************/
+/***entity****/
+/*************/
+
+
+struct User {   
+    private :
+        string id;
+        string pwd;
+        string name;
+        string SSN;
+    public :
+        User(string id, string pwd, string, string SSN) {
+            id = this->id; 
+            pwd = this->pwd;
+            name = this->pwd;
+            SSN = this->SSN;
+        }
+};
+
+
+/*************/
+/*************/
+
+int LogStatus = 0; // 로그인 상태 전역 변수
+
+
+
+class SignUp // 회원가입 control
+{
+public:
+    void showResult() {
+        
+    }
+    
+};
+
+class SignUpUI // 회원가입 boundary
+{
+private:
+
+public:
+    void startInterface() {
+        fprintf(out_fp, "1.1. 회원가입\n");
+    }
+
+    int signup() {
+        string id, pwd, name, SSN;
+        // 입력 형식 : 이름, 주민번호, ID, Password를 파일로부터 읽음
+        fscanf(in_fp, "%s %s %s %s", name, SSN, id, pwd);
+
+        User newUser = User(name, SSN, id, pwd);
+
+        //showResult 호출
+
+        fprintf(out_fp, "%s %s %s %s\n", name, SSN, id, pwd);
+        return 1;
+    }
+};
+
+void program_exit() {
+
+}
+
+
+/*************/
+/****main*****/
+/*************/
+
+
 int main()
 {
     // 파일 입출력을 위한 초기화
@@ -45,8 +117,8 @@ void doTask()
             switch (menu_level_2)
             {
             case 1: // "1.1. 회원가입“ 메뉴 부분
-            { 
-                SignUp();
+            {
+                signup();
 
                 break;
             }
@@ -61,14 +133,14 @@ void doTask()
         {
             switch (menu_level_2)
             {
-                case 1: // "2.1. 로그인"
-                {
+            case 1: // "2.1. 로그인"
+            {
 
-                }
-                case 2: // "2.2. 로그아웃"
-                {
+            }
+            case 2: // "2.2. 로그아웃"
+            {
 
-                }
+            }
             }
         }
 
@@ -86,48 +158,5 @@ void doTask()
         }
         }
     }
-return;
-}
-
-
-/*************/
-/***entity****/
-/*************/
-
-
-struct User {   
-    private :
-        string id[MAX_STRING];
-        string pwd[MAX_STRING];
-        string name[MAX_STRING];
-    public :
-        
-};
-
-
-
-
-/*************/
-/*************/
-
-void SignUpUI() // 회원가입 boundary
-{
-    char user_type[MAX_STRING], name[MAX_STRING], SSN [MAX_STRING],address[MAX_STRING], ID[MAX_STRING], password[MAX_STRING];
-
-    // 입력 형식 : 이름, 주민번호, ID, Password를 파일로부터 읽음
-    fscanf(in_fp, "%s %s %s %s", name, SSN, ID, password);
-
-    void SignUp();
-
-    fprintf(out_fp, "1.1. 회원가입\n");
-    fprintf(out_fp, "%s %s %s %s\n", name, SSN, ID, password);
-}
-
-void SignUp() { // 회원가입 control
-
-}
-
-
-void program_exit() {
-
+    return;
 }
