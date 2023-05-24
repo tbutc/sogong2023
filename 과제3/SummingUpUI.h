f5117734af12b9356712a5eca0e266f657071c9e
dlfnfl1 <132897352+dlfnfl1@users.noreply.github.com>
2023-05-24 14:21:28 +0900

    이루리-통계출력(입력부 미완성)

---
+++ 과제3/SummingUpUI.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "SummingUp.h"
+using namespace std;
+
+class SummingUpUI {
+private:
+
+public:
+    SummingUpUI();
+    void startinterface(string name, int usertype, vector<Application> Applications);
+};
