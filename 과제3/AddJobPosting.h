5ae49f8f9f9471023c9b2488335cb34b32a6443d
dlfnfl1 <132897352+dlfnfl1@users.noreply.github.com>
2023-05-26 05:52:03 +0900

    Add files via upload

--- 과제3/AddJobPosting.h
+++ 과제3/AddJobPosting.h
@@ -7,9 +7,7 @@ using namespace std;
 class AddJobPosting
 {
 private:
-
-public:
-    
-    AddJobPosting(ifstream& inputFile, string cn, string id, string ssn, vector<JobPosting>& jobPostings);
     void Add(string cn, string id, string ssn, vector<JobPosting>& jobPostings, JobDetail jd);
+public:
+    AddJobPosting(ifstream& inputFile, string cn, string id, string ssn, vector<JobPosting>& jobPostings);
 };
