3d7d629de9133eda6b4132313f7db5365525a921
장은서 <124859156+megjangg@users.noreply.github.com>
2023-05-25 16:26:58 +0900

    Update editedApply.h

--- 과제3/editedApply.h
+++ 과제3/editedApply.h
@@ -1,10 +1,17 @@
-
-#include <iostream>
-#include <string>
+#ifndef APPLY_H
+#define APPLY_H
 #include <cstdio>
 #include <vector>
-#include "JobPosting.h"
 #include "ApplyInfo.h"
+#include "JobPosting.h"
+
+#include <ctime>
+
+struct Date {
+    int year;
+    int month;
+    int day;
+};
 
 using namespace std;
 
@@ -13,7 +20,40 @@ class Apply
 public:
     vector<ApplyInfo> start(string ssn, vector<JobPosting> jobPostings);
     vector<ApplyInfo> sortApplicationsByCompanyName(const vector<ApplyInfo>& applications);
-
+    void cancelApplication(string ssn, vector<Application>& applications);
 };
+
+class Application {
+private:
+    string companyName;
+    string registrationNumber;
+    string jobTitle;
+    int applicantLimit;
+    Date deadline;
+public:
+    Application(const std::string& cn, const std::string& rn, const std::string& jt, int al, const Date& dl)
+        : companyName(cn), registrationNumber(rn), jobTitle(jt), applicantLimit(al), deadline(dl) {}
+
+    string getCompanyName() const {
+        return companyName;
+    }
+
+    string getRegistrationNumber() const {
+        return registrationNumber;
+    }
+
+    string getJobTitle() const {
+        return jobTitle;
+    }
+
+    int getApplicantLimit() const {
+        return applicantLimit;
+    }
+
+    Date getDeadline() const {
+        return deadline;
+    }
+};
+#endif
 
  
