259df151f43816373946b977fb4b346eec0bab8b
장은서 <124859156+megjangg@users.noreply.github.com>
2023-05-25 17:49:52 +0900

    Update Application.h

--- 과제3/임시(이루리)/Application.h
+++ 과제3/임시(이루리)/Application.h
@@ -1,24 +1,39 @@
 #pragma once
-
-#include <string>
 #include "JobPosting.h"
-#include "User.h"
-
+#include <string>
+#include <ctime>
+using namespace std;
 
 
 class Application {
 private:
-    JobPosting applicationRecord;
-    string SSN;
+    string companyName;
+    string registrationNumber;
+    string jobTitle;
+    int applicantLimit;
+    Date deadline;
 
 public:
-    Application(JobPosting jp, string id);
-    void getApplication();
-    JobPosting getJobPosting() ;
+    Application(const std::string& cn, const std::string& rn, const std::string& jt, int al, const Date& dl)
+        : companyName(cn), registrationNumber(rn), jobTitle(jt), applicantLimit(al), deadline(dl) {}
 
-     string getssn()  {
-        return SSN;
-    }    
-    void showSortedApplication();
+    string getCompanyName() const {
+        return companyName;
+    }
+
+    string getRegistrationNumber() const {
+        return registrationNumber;
+    }
+
+    string getJobTitle() const {
+        return jobTitle;
+    }
+
+    int getApplicantLimit() const {
+        return applicantLimit;
+    }
+
+    Date getDeadline() const {
+        return deadline;
+    }
 };
-
