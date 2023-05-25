adfd974027153395afd425b65ae7aeead07b37a1
장은서 <124859156+megjangg@users.noreply.github.com>
2023-05-25 16:27:54 +0900

    Update editedApplyUI.h

--- 과제3/editedApplyUI.h
+++ 과제3/editedApplyUI.h
@@ -1,5 +1,12 @@
 #include "Apply.h"
+#include "JobPosting.h"
 
+#include <ctime>
+struct Date {
+    int year;
+    int month;
+    int day;
+};
 class ApplyUI
 {
 private:
@@ -8,4 +15,5 @@ public:
     ApplyUI();
     void showInterface(JobPosting jobposting);
     void showSortedApplications(const vector<ApplyInfo>& sortedApplications);
+    void cancelApplication(const string& ssn, vector<Application>& applications);
 };
