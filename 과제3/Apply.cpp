df3450218bafe4d98971e0d673bc9eeddd3909d4
장은서 <124859156+megjangg@users.noreply.github.com>
2023-05-26 02:51:32 +0900

    주석추가

--- 과제3/Apply.cpp
+++ 과제3/Apply.cpp
@@ -6,6 +6,12 @@
 using namespace std;
 
 
+/*
+    함수 이름 : Apply::start
+    기능	  : 사업자 번호와 채용 정보 목록을 전달받아 일치하는 채용 정보에 대한 지원정보값을 반환
+    전달 인자 : ssn, jobPosting
+    반환값    : Application 객체의 벡터 (vector<Application> applications)
+*/
 vector<Application> Apply::start(string ssn, vector<JobPosting> jobPostings) {
     vector<Application> applications;
     for (auto& posting : jobPostings) {
