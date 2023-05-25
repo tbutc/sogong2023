f2e2e8e31f8c8a3bffef3a3597064a8f85012cb0
Seonyoung <114154547+caprisun0@users.noreply.github.com>
2023-05-26 07:20:48 +0900

    Update ApplyUI.cpp

--- 과제3/ApplyUI.cpp
+++ 과제3/ApplyUI.cpp
@@ -9,7 +9,12 @@
 #include "JobPosting.h"
 
 using namespace std;
-
+/*
+함수 이름: ApplyUI::showInterface
+기능: 채용 지원 인터페이스를 표시하고, 사업자 등록 번호를 입력 받는 함수
+전달 인자: inputFile -> ifstream 객체
+반환값: ssn
+*/
 ApplyUI::ApplyUI() {}
 
 string ApplyUI::showInterface(ifstream& inputFile) {
