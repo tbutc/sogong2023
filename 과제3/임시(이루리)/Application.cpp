11bd02a634610056fe7464e5fc43ec8f2c4058f4
장은서 <124859156+megjangg@users.noreply.github.com>
2023-05-25 17:50:41 +0900

    Update Application.cpp

--- 과제3/임시(이루리)/Application.cpp
+++ 과제3/임시(이루리)/Application.cpp
@@ -1,53 +1,28 @@
-#include "Application.h"
 #include <iostream>
-#include <algorithm>
+using namespace std;
 #include <vector>
-#include "User.h"
-#include <fstream>
+#include "Application.h"
 
+Application::Application(const string& cn, const string& rn, const string& jt, const int al, const Date& dl)
+    : companyName(cn), registrationNumber(rn), jobTitle(jt), applicantLimit(al), deadline(dl) {}
 
-Application::Application(JobPosting jp, string ssn) :
-    applicationRecord(jp), SSN(ssn)
-{
-
+string Application::getCompanyName() const {
+    return companyName;
 }
 
-JobPosting Application:: getJobPosting() {
-    return applicationRecord;
+string Application::getRegistrationNumber() const {
+    return registrationNumber;
 }
 
-
-/*
-/정렬위해 bool로 비교/
-
-bool compareJobPosting( JobPosting a,  JobPosting b) {
-    return (a.getJobPostingcname()) < (b.getJobPostingcname());
+string Application::getJobTitle() const {
+    return jobTitle;
 }
 
-/정렬 여기서 getJobposting이랑 getApplication 함수 같이 사용하여 연결. /
-
-void showSortedApplication( User user,  vector<Application> applications,  vector<JobPosting> jobPostings) {
-    vector<Application> userApplications = getApplication(user, applications);
-    vector<JobPosting> sortedJobPostings;
-
-    for ( Application application : userApplications) {
-        JobPosting jobPosting = getJobPosting(application, jobPostings);
-        sortedJobPostings.push_back(jobPosting);
-    }
-
-    sort(sortedJobPostings.begin(), sortedJobPostings.end(), compareJobPosting);
-
-    ofstream outputFile("output.txt");
-    if (outputFile.is_open()) {
-        for ( JobPosting job : sortedJobPostings) {
-            JobDetail detail = job.getJobPostingDetail();
-            outputFile << (job.getJobPostingcname()) << " " << detail.JobTitle << " " << detail.applicantLimit << " " << detail.deadline.year << "/" << detail.deadline.month << "/" << detail.deadline.day << endl; //사업자 번호 추가
-        }
-        outputFile.close();
-    }
-    else {
-        cout << "파일을 열 수 없습니다." << endl;
-    }
+int  Application:: getApplicantLimit() const {
+    return applicantLimit;
 }
 
-*/
+Date  Application::getDeadline() const {
+    return deadline;
+}
+
