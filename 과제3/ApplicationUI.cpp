d635a5fa9f80982d9d932968bfe9f45a0b29394e
장은서 <124859156+megjangg@users.noreply.github.com>
2023-05-26 02:50:16 +0900

    주석추가

--- 과제3/ApplicationUI.cpp
+++ 과제3/ApplicationUI.cpp
@@ -5,7 +5,12 @@
 #include <vector>
 #include "ApplicationUI.h"
 #include "ListApplication.h"
-
+/* 
+    함수 이름: ApplicationUI::showSortedApplications
+    기능: 정렬된 지원 정보를 출력.
+    전달 인자: sortedApplications (정렬된 Application 객체들의 벡터)
+    반환값: 없음
+*/
 void ApplicationUI::showSortedApplications(vector<Application> sortedApplications) {
     ofstream outputFile("output.txt", std::ios::app);
     outputFile << "\n4.3 지원정보 조회:\n";
@@ -34,7 +39,12 @@ void ApplicationUI::showSortedApplications(vector<Application> sortedApplication
     }
     outputFile.close();
 }
-
+/*
+    함수 이름: ApplicationUI::PrintcancelApplication
+    기능: 검색된 ssn(사업자 번호)에 대힌 지원 정보를 취소
+    전달 인자: ssn, applications: 
+    반환값: 없음  
+*/
 void ApplicationUI::PrintcancelApplication(string ssn, vector<Application> applications) {
     ListApplication apply;
     apply.cancelApplication(ssn, applications);
